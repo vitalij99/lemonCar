5af1ac8722bfd155534f15e35bc9cbb5012f5f88
vitalij99 <vitalisklymko@gmail.com>
2023-08-28 15:33:07 +0300

    add header

---
+++ app/_componets/Container/Container.js
@@ -0,0 +1,7 @@
+import style from "./container.module.scss";
+
+const Container = ({ children }) => {
+    return <div className={style.container}>{children}</div>;
+};
+
+export default Container;
---
+++ app/_componets/Header/Header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+import Logo from "@/public/images/logo.svg";
+import LogoSec from "@/public/images/logo-sec.svg";
+
+import Container from "../Container/Container";
+import style from "./header.module.scss";
+
+export const Header = () => {
+    return (
+        <header>
+            <Container>
+                <div className={style.wrapper}>
+                    <Link href="/">
+                        <div className={style.header_wrapp}>
+                            <Logo />
+                            <LogoSec />
+                        </div>
+                    </Link>
+                    <div className={style.navigation}>
+                        <Link href="/carlist">Car List</Link>
+                        <Link href="/viptransfer">VIP Transfer</Link>
+                        <Link href="/yachts">Yachts</Link>
+                        <Link href="/photshoots">Photshoots with car</Link>
+                        <Link href="/about">About Us</Link>
+                        <Link href="/contact">Contact Us</Link>
+                    </div>
+                </div>
+            </Container>
+        </header>
+    );
+};
