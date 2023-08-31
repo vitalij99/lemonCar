00a7260595603165a1e0d5fb615ef948a06cab92
vitalij99 <vitalisklymko@gmail.com>
2023-08-31 17:17:30 +0300

    add box

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import Hero from "./_componets/Hero/Hero";
 import Available from "./_componets/Available/Available";
 import Advantage from "./_componets/Advantage/Advantage";
+import Box from "./_componets/Box/Box";
 
 const heroFont = localFont({
     variable: "--font-monument",
@@ -14,6 +15,8 @@ export default function Home() {
             <Hero />
             <Available />
             <Advantage />
+            <Box />
+            {/* listcar */}
         </main>
     );
 }
