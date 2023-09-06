28c8c4f88902d53f17b83ec0552607b0fca212b9
vitalij99 <vitalisklymko@gmail.com>
2023-09-06 19:48:14 +0300

    add Footer

---
+++ app/_componets/Footer/Footer.js
@@ -0,0 +1,13 @@
+import style from './footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className={style.text}>
+        lemann • lemann • lemann • lemann • lemann • lemann
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,45 @@
-import localFont from "next/font/local";
+import localFont from 'next/font/local';
 
-import "./globals.css";
+import './globals.css';
 
-import { Header } from "./_componets/Header/Header";
+import { Header } from './_componets/Header/Header';
+import Footer from './_componets/Footer/Footer';
 
 const myFont = localFont({
-    variable: "--font-muller",
-    src: [
-        {
-            path: "../fonts/MullerRegular.ttf",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "../fonts/MullerMedium.ttf",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            path: "../fonts/MullerBold.ttf",
-            weight: "700",
-            style: "normal",
-        },
-    ],
+  variable: '--font-muller',
+  src: [
+    {
+      path: '../fonts/MullerRegular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MullerMedium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MullerBold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
 });
 
 export const metadata = {
-    title: "Limon Car",
-    description: "Limon Car ren",
+  title: 'Limon Car',
+  description: 'Limon Car ren',
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={myFont.variable}>
-                <Header />
+  return (
+    <html lang="en">
+      <body className={myFont.variable}>
+        <Header />
 
-                {children}
-            </body>
-        </html>
-    );
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
