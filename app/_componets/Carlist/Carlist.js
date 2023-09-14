809977c79cebfc3b331132138a1ce3d35e74fac3
vitalij99 <vitalisklymko@gmail.com>
2023-09-14 22:26:51 +0300

    add carlist arr

--- app/_componets/Carlist/Carlist.js
+++ app/_componets/Carlist/Carlist.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import CarCard from '../CarCard/CarCard';
 import Loading from '../Loading/Loading';
+import style from './carlist.module.scss';
 
 const data = [
   {
@@ -11,6 +12,16 @@ const data = [
     power: 671,
     engine: 3,
     id: 'asdasdasd',
+    image:
+      'https://res.cloudinary.com/dizrnyvqx/image/upload/v1693403682/cld-sample-5.jpg',
+  },
+  {
+    name: 'McLaren Artura',
+    prise: 1520,
+    seats: 22,
+    power: 71,
+    engine: 43,
+    id: 'asdasdassd',
     image:
       'https://res.cloudinary.com/dizrnyvqx/image/upload/v1693403682/cld-sample-5.jpg',
   },
@@ -25,7 +36,7 @@ const Carlist = () => {
   return (
     <>
       {list.length > 0 ? (
-        <ul>
+        <ul className={style.list}>
           {list?.map((car, i) => (
             <li key={i}>
               <CarCard car={car} />
