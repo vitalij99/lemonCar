36c280685f657f2ec95c38d50da609e8def5e2e6
vitalij99 <vitalisklymko@gmail.com>
2023-09-16 20:39:19 +0300

    get list card

--- app/_componets/Carlist/Carlist.js
+++ app/_componets/Carlist/Carlist.js
@@ -1,37 +1,10 @@
-'use client';
-import { useEffect, useState } from 'react';
 import CarCard from '../CarCard/CarCard';
 import Loading from '../Loading/Loading';
 import style from './carlist.module.scss';
+import { getCarList } from '@/lib/carList';
 
-const data = [
-  {
-    name: 'McLaren Artura',
-    prise: 520,
-    seats: 2,
-    power: 671,
-    engine: 3,
-    id: 'asdasdasd',
-    image:
-      'https://res.cloudinary.com/dizrnyvqx/image/upload/v1693403682/cld-sample-5.jpg',
-  },
-  {
-    name: 'McLaren Artura',
-    prise: 1520,
-    seats: 22,
-    power: 71,
-    engine: 43,
-    id: 'asdasdassd',
-    image:
-      'https://res.cloudinary.com/dizrnyvqx/image/upload/v1693403682/cld-sample-5.jpg',
-  },
-];
-
-const Carlist = () => {
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    setList(data);
-  }, []);
+const Carlist = async () => {
+  const list = await getCarList();
 
   return (
     <>
