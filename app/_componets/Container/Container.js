a2b83092cf42c12794986fad454c81384ae15006
vitalij99 <vitalisklymko@gmail.com>
2023-09-19 18:31:57 +0300

    add hero des

--- app/_componets/Container/Container.js
+++ app/_componets/Container/Container.js
@@ -1,7 +1,7 @@
-import style from "./container.module.scss";
+import style from './container.module.scss';
 
 const Container = ({ children }) => {
-    return <div className={style.container}>{children}</div>;
+  return <div className={style.container}>{children}</div>;
 };
 
 export default Container;
