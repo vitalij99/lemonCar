be43efdf1397c52a611e90ebd1b436557943b8f4
vitalij99 <vitalisklymko@gmail.com>
2023-09-19 19:12:32 +0300

    add Advantage to desk

--- app/_componets/Advantage/Advantage.js
+++ app/_componets/Advantage/Advantage.js
@@ -1,31 +1,42 @@
-import style from "./advantage.module.scss";
-import Container from "../Container/Container";
+import style from './advantage.module.scss';
+import Container from '../Container/Container';
 
 const Advantage = () => {
-    return (
-        <section className={style.section}>
-            <Container>
-                <h3 className={style.title}>WHY CHOOSE US</h3>
-                <h2 className={`${style.title_sec} ${style.title_adv}`}>
-                    We offer the best experience with our rental deals offer the
-                    best
-                </h2>
-                <h3 className={style.advantage_title}>FREECAR DELIVERY</h3>
-                <p className={style.advantage_para}>
-                    We will deliver the car directly to your location in Dubai
-                </p>
-                <h3 className={style.advantage_title}>EXPERIENCEDDRIVERS</h3>
-                <p className={style.advantage_para}>
-                    Don’t have a driver? Don’t worry, we have many experienced
-                    drivers for you.
-                </p>
-                <h3 className={style.advantage_title}>TECHNICAL SUPPORT</h3>
-                <p className={style.advantage_para}>
-                    Have a question? Contact support when you have a problem.
-                </p>
-            </Container>
-        </section>
-    );
+  return (
+    <section className={style.section}>
+      <Container>
+        <div className={style.wrapp}>
+          <div>
+            <h3 className={style.title}>WHY CHOOSE US</h3>
+            <h2 className={`${style.title_sec} ${style.title_adv}`}>
+              We offer the best experience with our rental deals offer the best
+            </h2>
+          </div>
+          <ul>
+            <li>
+              <h3 className={style.advantage_title}>FREECAR DELIVERY</h3>
+              <p className={style.advantage_para}>
+                We will deliver the car directly to your location in Dubai
+              </p>
+            </li>
+            <li>
+              <h3 className={style.advantage_title}>EXPERIENCEDDRIVERS</h3>
+              <p className={style.advantage_para}>
+                Don’t have a driver? Don’t worry, we have many experienced
+                drivers for you.
+              </p>
+            </li>
+            <li>
+              <h3 className={style.advantage_title}>TECHNICAL SUPPORT</h3>
+              <p className={style.advantage_para}>
+                Have a question? Contact support when you have a problem.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default Advantage;
