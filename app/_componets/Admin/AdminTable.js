da4a6975abde5000fc77ce1f983e48624c3fa219
vitalij99 <vitalisklymko@gmail.com>
2023-09-22 18:23:02 +0300

    add tablet

--- app/_componets/Admin/AdminTable.js
+++ app/_componets/Admin/AdminTable.js
@@ -1,96 +1,60 @@
 'use client';
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+
 import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+
+import './adminTable.scss';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'carBrand', label: 'Brand', minWidth: 100 },
-  { id: 'engine', label: 'Engine', minWidth: 100 },
-  { id: 'power', label: 'Power', minWidth: 100 },
-  { id: 'prise', label: 'Prise', minWidth: 100 },
-  { id: 'seats', label: 'Seats', minWidth: 100 },
-  { id: 'image', label: 'Image', minWidth: 40 },
+  { field: 'name', editable: true, headerName: 'Name', minWidth: 170 },
+  { field: 'carBrand', editable: true, headerName: 'Brand', minWidth: 100 },
+  { field: 'engine', editable: true, headerName: 'Engine', minWidth: 100 },
+  { field: 'power', editable: true, headerName: 'Power', minWidth: 100 },
+  { field: 'prise', editable: true, headerName: 'Prise', minWidth: 100 },
+  { field: 'seats', editable: true, headerName: 'Seats', minWidth: 100 },
+  {
+    field: 'image',
+    editable: true,
+    headerName: 'Image',
+    minWidth: 40,
+    renderCell: params => {
+      return (
+        <Image src={params.row.image} alt="avatar" width={40} height={40} />
+      );
+    },
+  },
 ];
 
 export default function AdminTable({ params }) {
-  const [rows, setRows] = React.useState(null);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rows, setRows] = useState(null);
+  const [brand, setBrand] = useState(null);
 
-  React.useEffect(() => {
-    const list = async () => (await axios(`/api/admin/${params}`)) ?? [];
-    list().then(result => setRows(result.data));
+  useEffect(() => {
+    (async () => {
+      const result = await axios(`/api/admin/${params}`);
+      setRows(result.data);
+    })();
   }, [params]);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   if (!rows) {
     return <h1>Loading</h1>;
   }
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map(row => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                    {columns.map(column => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        className="tablet"
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
       />
-    </Paper>
+    </div>
   );
 }
