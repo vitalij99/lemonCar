108a540a1e858a01ecd9f065067e4477414b32e2
vitalij99 <vitalisklymko@gmail.com>
2023-09-22 23:07:20 +0300

    add POST car

--- app/api/brand/route.js
+++ app/api/brand/route.js
@@ -1,6 +1,26 @@
 import { db } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
+export async function POST(req) {
+  try {
+    const data = await req.json();
+    if (!data) return NextResponse('wrong');
+
+    const { brand } = data;
+
+    const newBrand = await db.carList.create({
+      data: {
+        brand,
+      },
+    });
+
+    return NextResponse.json(newBrand);
+  } catch (error) {
+    console.log('[SERVERS_POST]', error);
+    return new NextResponse(error.message, { status: 500 });
+  }
+}
+
 export async function GET(req) {
   try {
     const result = await db.brand.findMany();
--- app/api/carlist/route.js
+++ app/api/carlist/route.js
@@ -1,6 +1,27 @@
 import { db } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
+export async function POST(req) {
+  try {
+    const car = await req.json();
+    if (!car) return NextResponse('wrong');
+
+    const newCar = await db.carList.create({
+      data: {
+        ...car,
+      },
+      include: {
+        brand: car.carBrand,
+      },
+    });
+
+    return NextResponse.json(newCar);
+  } catch (error) {
+    console.log('[SERVERS_POST]', error);
+    return new NextResponse(error.message, { status: 500 });
+  }
+}
+
 export async function PATCH(req) {
   try {
     const data = await req.json();
