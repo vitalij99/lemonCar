5f81ed363df4d53afb9800e8d320af6bea90a9c9
vitalij99 <vitalisklymko@gmail.com>
2023-09-25 22:14:12 +0300

    add calender

--- app/_componets/RentalCar/RentalCar.js
+++ app/_componets/RentalCar/RentalCar.js
@@ -1,27 +1,49 @@
 'use client';
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Box,
-} from '@mui/material';
-import { GridExpandMoreIcon } from '@mui/x-data-grid';
+import { Box } from '@mui/material';
 
-import { useState } from 'react';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useEffect, useState } from 'react';
+import styled from './RentalCar.module.scss';
+import dayjs from 'dayjs';
 
 const RentalCar = ({ carData }) => {
-  const [calendar, setCalendar] = useState(new Date());
+  const [calendar, setCalendar] = useState({
+    start: dayjs(),
+    end: dayjs(),
+  });
+
+  useEffect(() => {
+    const diffInDays = calendar.end.diff(calendar.start, 'day');
+    console.log(diffInDays);
+  }, [calendar]);
+
   return (
-    <Box>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<GridExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <h1>Accordion 1</h1>
-        </AccordionSummary>
-      </Accordion>
+    <Box
+      sx={{
+        p: '55px',
+        backgroundColor: 'color-mix(in lch, var(--background) 80%, #3d3d3d )',
+        border: '1px solid #2F2F2F',
+      }}
+    >
+      <h1>RENTAL CAR</h1>
+
+      <Box className={styled.data}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Start dates"
+            value={calendar.start}
+            minDate={dayjs()}
+            onChange={newValue => setCalendar({ ...calendar, start: newValue })}
+          />
+          <DatePicker
+            label="End"
+            value={calendar.end}
+            minDate={dayjs()}
+            onChange={newValue => setCalendar({ ...calendar, end: newValue })}
+          />
+        </LocalizationProvider>
+      </Box>
     </Box>
   );
 };
--- app/carlist/[carid]/page.js
+++ app/carlist/[carid]/page.js
@@ -1,9 +1,12 @@
 import Container from '@/app/_componets/Container/Container';
 
-import style from '../../styles/carlist.module.scss';
+import styleTitle from '../../styles/carlist.module.scss';
+import style from '../../styles/carpage.module.scss';
 import BreadcrumbsCustl from '@/app/_componets/Breadcrumbs/Breadcrumbs';
 import { getCarByID } from '@/lib/carList';
 import RentalCar from '@/app/_componets/RentalCar/RentalCar';
+import { Box } from '@mui/material';
+import Image from 'next/image';
 
 const Car = async ({ params }) => {
   const carData = await getCarByID(params.carid);
@@ -13,11 +16,20 @@ const Car = async ({ params }) => {
   }
 
   return (
-    <section className={style.section}>
+    <section className={styleTitle.section}>
       <Container>
         <BreadcrumbsCustl carName={carData.name} />
-        <h1 className={style.title}>{carData.name}</h1>
-        <RentalCar carData={carData} />
+        <h1 className={styleTitle.title}>{carData.name}</h1>
+        <div className={style.wrapp}>
+          <Image
+            className={style.image}
+            src={carData.image}
+            alt="car"
+            width={345}
+            height={125}
+          />
+          <RentalCar carData={carData} />
+        </div>
       </Container>
     </section>
   );
