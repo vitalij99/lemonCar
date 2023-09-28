46a0924acaa9685877c719e2822a912ffbd2dfc6
vitalij99 <vitalisklymko@gmail.com>
2023-09-28 20:17:17 +0300

    api add image

--- app/api/admin/[category]/route.js
+++ app/api/admin/[category]/route.js
@@ -1,5 +1,15 @@
 import { db } from '@/lib/db';
 import { NextResponse } from 'next/server';
+import cloudinary from 'cloudinary';
+
+const url = 'https://api.cloudinary.com/v1_1/dizrnyvqx/image/upload';
+
+cloudinary.config({
+  cloud_name: 'dizrnyvqx',
+  api_key: '864124585882834',
+  api_secret: 'aTZfRMUGZ_JeaUtDn1cokgBJ--o',
+  secure: true,
+});
 
 export async function GET(req) {
   try {
@@ -11,15 +21,35 @@ export async function GET(req) {
     return new NextResponse('Internal Error', { status: 500 });
   }
 }
+
 export async function POST(req) {
   try {
     const formData = await req.formData();
 
+    const images = formData.getAll('image');
     const name = formData.get('name');
-    console.log(formData);
-    return NextResponse.json({ name });
 
-    return NextResponse.json(res);
+    const results = [];
+    const cloudFormData = new FormData();
+
+    for (const image of images) {
+      cloudFormData.append('file', image);
+      cloudFormData.append('upload_preset', 'ojlqsb85');
+
+      const response = await fetch(url, {
+        method: 'POST',
+        body: cloudFormData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      results.push(data.url);
+    }
+
+    return NextResponse.json(results, { status: results.status });
   } catch (error) {
     console.log('[SERVERS_POST]', error);
     return new NextResponse('Internal Error', { status: 500 });
