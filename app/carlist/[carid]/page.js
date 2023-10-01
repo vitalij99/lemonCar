2bdbd0e0eca9dc0525148b3d04930a060bebb657
vitalij99 <vitalisklymko@gmail.com>
2023-10-01 19:32:24 +0300

    add title to car page

--- app/carlist/[carid]/page.js
+++ app/carlist/[carid]/page.js
@@ -1,12 +1,11 @@
+import RentalCar from '@/app/_componets/RentalCar/RentalCar';
+import { getCarByID } from '@/lib/carList';
+import BreadcrumbsCustl from '@/app/_componets/Breadcrumbs/Breadcrumbs';
+import IconSwiper from '@/app/_componets/IconSwiper/IconSwiper';
 import Container from '@/app/_componets/Container/Container';
 
 import styleTitle from '../../styles/carlist.module.scss';
 import style from '../../styles/carpage.module.scss';
-import BreadcrumbsCustl from '@/app/_componets/Breadcrumbs/Breadcrumbs';
-import { getCarByID } from '@/lib/carList';
-import RentalCar from '@/app/_componets/RentalCar/RentalCar';
-
-import IconSwiper from '@/app/_componets/IconSwiper/IconSwiper';
 
 const Car = async ({ params }) => {
   const carData = await getCarByID(params.carid);
@@ -24,6 +23,8 @@ const Car = async ({ params }) => {
           <IconSwiper images={carData.image} />
           <RentalCar carData={carData} />
         </div>
+        <h3 className={styleTitle.title}>WHY CHOOSE US</h3>
+        <h3 className={style.title}>Why you should rent {carData.name}</h3>
       </Container>
     </section>
   );
