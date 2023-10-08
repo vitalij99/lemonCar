c206fd4c432c8b988c05a14bb3a0ef1b270c0d78
vitalij99 <vitalisklymko@gmail.com>
2023-10-08 16:53:01 +0300

    add contact to contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import style from '@/app/styles/carpage.module.scss';
+import Contact from '@/app/_componets/Contact/Contact';
+import Contacts from '@/app/_componets/Contacts/Contacts';
 
 const page = () => {
-  return <div>contact</div>;
+  return (
+    <section className={style.section}>
+      <Contact />
+      <Contacts />
+    </section>
+  );
 };
 
 export default page;
