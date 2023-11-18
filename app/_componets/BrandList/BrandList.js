10b04f2990f2bd0b50393da943c61436181986b6
vitalij99 <vitalisklymko@gmail.com>
2023-11-18 15:46:04 +0200

    BrandList styls

--- app/_componets/BrandList/BrandList.js
+++ app/_componets/BrandList/BrandList.js
@@ -3,22 +3,27 @@ import { Box, Link, List, ListItem } from '@mui/material';
 
 import React from 'react';
 
+const ALL_BRANDS = { id: '', name: 'All' };
+
 const BrandList = async () => {
   const brands = await getBrand();
-
+  brands.unshift(ALL_BRANDS);
   return (
     <Box>
       {brands && (
-        <List sx={{ display: 'flex', justifyContent: 'center' }}>
-          <ListItem sx={{ maxWidth: '100px' }}>
-            <Link sx={{ p: '20px' }} href={`/carlist`}>
-              All
-            </Link>
-          </ListItem>
-
+        <List
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
           {brands.map((brand, i) => (
             <ListItem sx={{ width: 'max-content' }} key={i}>
-              <Link sx={{ p: '20px' }} href={`/carlist/?search=${brand.id}`}>
+              <Link
+                sx={{ p: '20px', color: 'var(--text)', fontSize: '20px' }}
+                href={brand.id ? `/carlist/?search=${brand.id}` : `/carlist`}
+                underline="false"
+              >
                 {brand.name}
               </Link>
             </ListItem>
