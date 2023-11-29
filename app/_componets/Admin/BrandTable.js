576f1c3ef2fa89711eb2705380887b1d26461f26
vitalij99 <vitalisklymko@gmail.com>
2023-11-29 19:54:19 +0200

    add new brend

--- app/_componets/Admin/BrandTable.js
+++ app/_componets/Admin/BrandTable.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { newColumnsBrand } from '@/lib/columns';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { Button, ThemeProvider, createTheme } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -11,10 +11,16 @@ const theme = createTheme({
     mode: 'dark',
   },
 });
-
+const NEW_BRANDINFO = [
+  {
+    id: 'new',
+    name: 'name',
+  },
+];
 const BrandTable = () => {
   const [brands, setBrands] = useState(null);
   const [columns, setColumns] = useState([]);
+  const [newBrands, setNewBrands] = useState(NEW_BRANDINFO);
 
   useEffect(() => {
     (async () => {
@@ -24,26 +30,52 @@ const BrandTable = () => {
   }, []);
 
   useEffect(() => {
-    setColumns(() => newColumnsBrand());
+    setColumns(() => newColumnsBrand({ handleUpdateImage }));
   }, []);
 
-  const handleProcessRowUpdate = async updatedRow => {
-    // push
-    console.log(updatedRow);
+  const handleUpdateImage = (event, id) => {
+    if (!event.target.files) {
+      return;
+    }
+    const file = event.target.files[0];
 
+    const newImageUrl = URL.createObjectURL(file);
+
+    if (id === 'new') {
+      setNewBrands(prev => [
+        { ...prev[0], logo: newImageUrl, imageFile: file },
+      ]);
+    } else {
+    }
+  };
+  const handleAddNewBrand = async () => {
+    console.log(newBrands[0]);
+    const { imageFile, name } = newBrands[0];
+    const formData = new FormData();
+
+    formData.append('logo', imageFile);
+    formData.append('name', name);
+
+    const { data } = await axios.post(`/api/admin/brand`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    setBrands(prev => [...prev, data]);
+  };
+  const handleProcessRowUpdate = async updatedRow => {
     const formData = new FormData();
 
     formData.append('id', updatedRow.id);
     formData.append('name', updatedRow.name);
-
+    // push
     await axios.patch(`/api/admin/brand`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     });
-
-    // await axios.patch(`/api/admin/brand`, newCar);
   };
+
   const handleProcessRowUpdateError = err => {};
   if (!brands) {
     return <h1>Loading</h1>;
@@ -75,18 +107,27 @@ const BrandTable = () => {
             onProcessRowUpdateError={handleProcessRowUpdateError}
           />
         </div>
-        {/* <h1>ADD new</h1>
+        <h1>ADD new</h1>
         <DataGrid
           className="tablet"
           rows={newBrands}
           columns={columns}
-          processRowUpdate={(updatedRow, originalRow) => {
-            setNewCar([{ ...updatedRow }]);
-          }}
           hideFooter={true}
+          processRowUpdate={updatedRow => {
+            setNewBrands([updatedRow]);
+          }}
+          initialState={{
+            columns: {
+              ...brands.initialState?.columns,
+              columnVisibilityModel: {
+                id: false,
+                actions: false,
+              },
+            },
+          }}
           onProcessRowUpdateError={handleProcessRowUpdateError}
         />
-        <Button onClick={handleAddNewCar}>Add new car</Button> */}
+        <Button onClick={handleAddNewBrand}>Add new brand</Button>
       </ThemeProvider>
     </div>
   );
