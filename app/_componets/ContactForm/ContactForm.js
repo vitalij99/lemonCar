44c5c2465bea171a9415a0bad9a10b8fc5e91c92
vitalij99 <vitalisklymko@gmail.com>
2023-12-05 17:36:58 +0200

    add vipTransfer to formsubmit

--- app/_componets/ContactForm/ContactForm.js
+++ app/_componets/ContactForm/ContactForm.js
@@ -1,12 +1,18 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import style from './contactForm.module.scss';
 import axios from 'axios';
 
 const ContactForm = ({ carForm }) => {
   const [comment, setComment] = useState('');
+  const [vipTransfer, setVipTransfer] = useState(null);
+  const [openTransfer, setOpenTransfer] = useState(false);
+
+  useEffect(() => {
+    axios('/api/viptransfer').then(({ data }) => setVipTransfer(data));
+  }, []);
 
   const onSubmit = async event => {
     event.preventDefault();
@@ -14,10 +20,12 @@ const ContactForm = ({ carForm }) => {
     const formData = new FormData(event.target);
     const phone = formData.get('phone');
     const comment = formData.get('comment');
+    const transferId = formData.get('transferId');
 
     await axios.post('/api/formsubmit', {
       phone,
       comment,
+      transferId,
       ...carForm,
     });
   };
@@ -43,8 +51,30 @@ const ContactForm = ({ carForm }) => {
         value={comment}
         onChange={handleComment}
       />
+      <div className={style.wrapp}>
+        <h3 className={style.text}>Choose a driver</h3>
+        <input
+          className={style.checkbox}
+          type="checkbox"
+          name="openTransfer"
+          onChange={() => {
+            setOpenTransfer(!openTransfer);
+          }}
+        />
+      </div>
+      {vipTransfer && openTransfer && (
+        <select name="transferId">
+          {vipTransfer.map(transfer => (
+            <option key={transfer.id} value={transfer.id}>
+              {transfer.name}
+            </option>
+          ))}
+        </select>
+      )}
 
-      <button type="submit">{carForm ? 'RENTAL CAR' : 'Contact Us'}</button>
+      <button className={style.btn} type="submit">
+        {carForm ? 'RENTAL CAR' : 'Contact Us'}
+      </button>
     </form>
   );
 };
