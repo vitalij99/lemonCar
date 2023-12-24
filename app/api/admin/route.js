74afef69dd602658a7715b4fbb2723e3d8e3e17e
vitalij99 <vitalisklymko@gmail.com>
2023-12-24 16:13:05 +0200

    add PATCH admin

--- app/api/admin/route.js
+++ app/api/admin/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
 
 import { db } from '@/lib/db';
+import { authUser } from '@/lib/auth';
 
 // password hash
 export async function POST(req) {
@@ -33,6 +34,25 @@ export async function POST(req) {
     }
 
     return new NextResponse('wrong', { status: 401 });
+  } catch (error) {
+    console.log('[SERVERS_POST]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+export async function PATCH(req) {
+  try {
+    const admin = authUser(req);
+    const { password: passwordUser, login: loginUser } = await req.json();
+
+    if (!passwordUser || !loginUser)
+      return NextResponse('wrong', { status: 401 });
+
+    const newAdmin = await db.admin.update({
+      where: { id: admin.id },
+      data: { login, password },
+    });
+
+    return NextResponse.json(newAdmin);
   } catch (error) {
     console.log('[SERVERS_POST]', error);
     return new NextResponse('Internal Error', { status: 500 });
