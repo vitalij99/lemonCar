d4f4cc9847c71651f4ce6b03983ac9468df84665
vitalij99 <vitalisklymko@gmail.com>
2024-02-11 15:06:15 +0200

    rework formsubmit

--- app/api/formsubmit/route.js
+++ app/api/formsubmit/route.js
@@ -6,42 +6,38 @@ import { NextResponse } from 'next/server';
 export async function POST(req) {
   try {
     const { carId, dataFirst, dataLast, ...messageForm } = await req.json();
+
+    let commentData = { ...messageForm };
+
     if (carId) {
       const car = await getCarByID(carId);
-
       const price = Number(car.price);
       const deposit = price * CAR_DEPOSIT;
       const diffInDays = getDiffDays(dataFirst, dataLast);
-      const totalPrise =
+      const totalPrice =
         diffInDays <= 0
           ? price
           : procentraPrice(price, diffInDays) * diffInDays;
 
-      const newComment = await db.forma.create({
-        data: {
-          ...messageForm,
-          carId,
-          dataFirst,
-          dataLast,
-          price,
-          deposit,
-          totalPrise,
-          diffInDays,
-        },
-      });
-
-      return NextResponse.json(newComment);
-    } else {
-      const newComment = await db.forma.create({
-        data: {
-          ...messageForm,
-        },
-      });
-
-      return NextResponse.json(newComment);
+      // add to bd
+      commentData = {
+        ...messageForm,
+        carId,
+        dataFirst,
+        dataLast,
+        price,
+        deposit,
+        totalPrice,
+        diffInDays,
+      };
     }
 
-    // add to bd
+    const newComment = await db.forma.create({ data: commentData });
+
+    return new NextResponse.json({
+      comment: newComment.comment,
+      phone: newComment.phone,
+    });
   } catch (error) {
     console.log('[SERVERS_POST]', error);
     return new NextResponse(error.message, { status: 500 });
