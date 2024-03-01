3d3eb4e48fe97f5b81b59ed0d8a520e7e5f5843c
vitalij99 <vitalisklymko@gmail.com>
2024-03-01 15:12:29 +0200

    updata app/page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Hero from '@/app/_componets/Hero/Hero';
 import Available from '@/app/_componets/Available/Available';
 import Advantage from '@/app/_componets/Advantage/Advantage';
-import Box from '@/app/_componets/Box/Box';
+
 import Steps from '@/app/_componets/Steps/Steps';
 
 import Contact from '@/app/_componets/Contact/Contact';
