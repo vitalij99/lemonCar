9ea27aac127e481136eec094ccd1b218b2da0179
vitalij99 <vitalisklymko@gmail.com>
2024-04-03 18:16:46 +0300

    new link

--- app/_componets/Footer/Footer.js
+++ app/_componets/Footer/Footer.js
@@ -1,15 +1,10 @@
+import { HEADER_LINK } from '@/lib/link';
 import Contacts from '../Contacts/Contacts';
 import Container from '../Container/Container';
 import Dropdown from '../Dropdown/Dropdown';
 import LinkCircle from '../LinkCircle/LinkCircle';
 import style from './footer.module.scss';
 
-const CUSTOMERS = [
-  { href: '/carlist', name: 'Car List' },
-  { href: '/terms', name: 'Terms' },
-  { href: '/about', name: 'About Us' },
-  { href: '/contacts', name: 'Contacts' },
-];
 const CARLIST = [
   { href: '/carlist/popular', name: 'Most popular' },
   { href: '/carlist/suv', name: 'SUV' },
@@ -18,13 +13,12 @@ const CARLIST = [
 ];
 
 const Footer = () => {
-  // add image + pc ver.
   return (
     <footer className={style.footer}>
       <Container>
         <div className={style.wrapp}>
           <Dropdown title="For Customers">
-            <Contacts title="For Customers" list={CUSTOMERS} blank={false} />
+            <Contacts title="For Customers" list={HEADER_LINK} blank={false} />
           </Dropdown>
           <Dropdown title="Car List">
             <Contacts title="Car List" list={CARLIST} blank={false} />
